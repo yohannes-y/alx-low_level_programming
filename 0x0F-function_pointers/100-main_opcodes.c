8189679a14f331d3ad5cb55716ae8aae1fa68e44
yohannes-y <yohannesy1672@gmail.com>
2022-10-12 19:22:17 +0000

    advance

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -22,12 +22,7 @@ void print_opcodes(char *a, int n)
 	}
 	printf("\n");
 }
-/**
- * main - prints the opcodes of its own main function
- * @argc: integer
- * @argv: character
- * Return: always 0.
- */
+
 int main(int argc, char *argv[])
 {
 	int ae;
@@ -37,12 +32,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
+
 	ae = atoi(argv[1]);
+
 	if (ae < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+
 	print_opcodes((char *)&main, ae);
 
 	return (0);
