62fa66d8192ff9ac332d058322ec0c1a398c13f7
yohannes-y <yohannesy1672@gmail.com>
2022-12-08 18:53:40 +0000

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of elements in the dlistint_t list.
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-	unsigned int count = 0;
-	const dlistint_t *list;
+	size_t nodes = 0;
 
-	list = malloc(sizeof(dlistint_t));
-	list = h;
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
 
-	if (list == NULL)
-	{
-		return (0);
-	}
-	while (list)
-	{
-		list = list->next;
-		count++;
-	}
-	return (count);
+	return (nodes);
 }
