9c307f4a5324e53fd6ac4245a000cfc89ad1d0f9
yohannes-y <yohannesy1672@gmail.com>
2022-12-08 19:19:37 +0000

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  *  * free_dlistint - Frees a linked dlistint_t list.
  *   * @head: The head of the dlistint_t list.
- *    */
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
