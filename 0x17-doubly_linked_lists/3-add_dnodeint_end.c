d50a23b681926123e6635c23db89edf5a2d7a98e
yohannes-y <yohannesy1672@gmail.com>
2022-12-08 19:04:33 +0000

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always EXIT_SUCCESS.
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *list = NULL, *end = *head;
