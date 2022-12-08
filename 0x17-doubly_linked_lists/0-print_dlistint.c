e79366d76148a59541c676e22399817d8d160d60
yohannes-y <yohannesy1672@gmail.com>
2022-12-08 18:08:52 +0000

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,15 @@
 #include "lists.h"
-
 size_t print_dlistint(const dlistint_t *h)
 {
+
 	unsigned int count = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
